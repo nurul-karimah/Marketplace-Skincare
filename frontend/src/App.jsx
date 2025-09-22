7ebb6eb678e004c41733d3f44136acc4bbe77516
nurul-karimah <nkarimah915@gmail.com>
2025-09-23 00:12:57 +0700

    tamplate sudah nempel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 // import TaskForm from './components/TaskForm';
 // import TaskList from './components/TaskList';
 // import TaskEdit from './components/TasksEdit';
-import Home from './components/TaskDetail';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <div className="container mt-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
