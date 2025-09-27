cec18ea813b3dfe3ce86a1f35a69a76c436769a0
nurul-karimah <nkarimah915@gmail.com>
2025-09-27 22:12:05 +0700

    membuat halaman menu sampai selesai

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-// import TaskForm from './components/TaskForm';
-// import TaskList from './components/TaskList';
-// import TaskEdit from './components/TasksEdit';
 import Home from './components/Home';
+import Menu from './components/menu'; // pastikan huruf besar sesuai
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/index" element={<Home />} />
+      <Route path="/menu" element={<Menu />} />
     </Routes>
   );
 }
